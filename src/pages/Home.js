40e32a549dd2ae2c27a90d91c91d04db4343d83e
srini1992-code <srinidhis1992dec@gmail.com>
2021-01-15 01:48:58 +0530

    deployed app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,32 +6,32 @@ import { Avatar } from '@material-ui/core';
 import AppsIcon from '@material-ui/icons/Apps';
 
 function Home() {
-  return (
-    <div className="home">
-      <div className="home__header">
-        <div className="home__headerLeft">
-          <Link to="/about">About</Link>
-          <Link to="/store">Store</Link>
-        </div>
-        <div className="home__headerRight">
-          <Link to="/gmail">Gmail</Link>
-          <Link to="/images">Images</Link>
-          <AppsIcon />
-          <Avatar />
-        </div>
-      </div>
-      <div className="home__body">
-        <img
-          src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
-          alt=""
-        />
-        <div className="home__inputContainer">
-          <Search hideButtons />
-          {/* <Search /> */}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="home">
+			<div className="home__header">
+				<div className="home__headerLeft">
+					<Link to="/about">About</Link>
+					<Link to="/store">Store</Link>
+				</div>
+				<div className="home__headerRight">
+					<Link to="/gmail">Gmail</Link>
+					<Link to="/images">Images</Link>
+					<AppsIcon />
+					<Avatar />
+				</div>
+			</div>
+			<div className="home__body">
+				<img
+					src="https://www.google.co.in/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+					alt=""
+				/>
+				<div className="home__inputContainer">
+					<Search />
+					{/* <Search /> */}
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
