3d1a07cf40d3c5b8044875035a516bb089aced43
srini1992-code <srinidhis1992dec@gmail.com>
2021-01-14 23:53:17 +0530

    styling searchPage__styling done

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -5,6 +5,12 @@ import useGoogleSearch from '../useGoogleSearch';
 import Response from '../response';
 import { Link } from 'react-router-dom';
 import Search from '../components/Search';
+import SearchIcon from '@material-ui/icons/Search';
+import DescriptionIcon from '@material-ui/icons/Description';
+import ImageIcon from '@material-ui/icons/Image';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+import RoomIcon from '@material-ui/icons/Room';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 function SearchPage() {
 	const [{ term }, dispatch] = useStateValue();
@@ -28,6 +34,43 @@ function SearchPage() {
 				{/* <h1>{term}</h1> */}
 				<div className="searchPage__headerBody">
 					<Search hideButtons />
+
+					<div className="searchPage__options">
+						<div className="searchPage__optionsLeft">
+							<div className="searchPage__option">
+								<SearchIcon />
+								<Link to="/all">All</Link>
+							</div>
+							<div className="searchPage__option">
+								<DescriptionIcon />
+								<Link to="/news">News</Link>
+							</div>
+							<div className="searchPage__option">
+								<ImageIcon />
+								<Link to="/images">Images</Link>
+							</div>
+							<div className="searchPage__option">
+								<LocalOfferIcon />
+								<Link to="/shopping">Shopping</Link>
+							</div>
+							<div className="searchPage__option">
+								<RoomIcon />
+								<Link to="/maps">Maps</Link>
+							</div>
+							<div className="searchPage__option">
+								<MoreVertIcon />
+								<Link to="/more">More</Link>
+							</div>
+						</div>
+						<div className="searchPage__optionsRight">
+							<div className="searchPage__option">
+								<Link to="/settings">Settings</Link>
+							</div>
+							<div className="searchPage__option">
+								<Link to="/tools">Tools</Link>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className="searchPage__results"></div>
